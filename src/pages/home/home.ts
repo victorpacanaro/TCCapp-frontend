523b1c4c6c1ca6f55534d3842499086d5d479747
Victor Contre Pacanaro <victor.contre@MIDGARD.LOCAL>
2020-09-26 22:48:41 -0300

    tela categorias

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,6 +10,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
-  }
+  } // o objeto navCtrl do tipo NavController está injetado na classe HomePage
 
+    login() {
+      this.navCtrl.push('CategoriasPage');
+    } // método login apontando para a página de categorias
 }
